e2cbdf259571593d6a9593502b1d22d7f789c8e7
Michel Nader <michelnader750@hotmail.com>
2024-04-02 17:59:40 -0300

    add application

---
+++ application/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	userId := "4da1fa03"
+	fmt.Println(userId)
+	var cents uint64 = 325
+	fmt.Println(cents)
+	varRate := 20.00
+	fmt.Println(varRate)
+	var port int
+	fmt.Println(port)
+}
---
+++ testVar/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var quantity int
+	// fmt.Println(quantity) // 0
+	// quantity = 42
+	// fmt.Println(quantity) // 42
+	// var quantity, stockId = 42, 52
+	// fmt.Println(quantity, stockId)
+	// quantity := 42
+	// fmt.Println(quantity)
+	var userID string
+	var cents int16
+	var varRate float32
+	var port int
+
+	userID = "4da1fa03"
+	cents = 325
+	varRate = 20
+
+	fmt.Println(userID, cents, varRate, port)
+
+}
